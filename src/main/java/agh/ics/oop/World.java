695d5e478bb169fa48fd869e1ae06b4975ea060b
antoniwojcik <antoniwojcik@student.agh.edu.pl>
2022-11-29 11:15:11 +0100

    lab 6 done

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -4,11 +4,11 @@ public class World {
     public static void main(String[] args) {
 
 
-       /*MoveDirection[] directions = new OptionsParser().parse(args);
+        MoveDirection[] directions = new OptionsParser().parse(args);
         Vector2d[] positions = { new Vector2d(2,2), new Vector2d(3,4), new Vector2d(11, 11)};
         IWorldMap map = new GrassField(10, positions);
         IEngine engine = new SimulationEngine(directions, map, positions);
-        engine.run();*/
+        engine.run();
 
 
         /*MoveDirection[] directions = new OptionsParser().parse(args);
